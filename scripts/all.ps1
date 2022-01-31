1480b4a97193c28e9e16aa7e6f84b89233bfd000
mattruma <mattruma@gmail.com>
2022-01-31 15:57:59 -0500

    added all.ps1

---
+++ scripts/all.ps1
@@ -0,0 +1,20 @@
+param(
+    [ValidatePattern('^[a-zA-Z0-9]+$')]
+    [Parameter(Mandatory = $true)] 
+    [String] $ProductId,
+    [String] $SubscriptionId,
+    [String] $Location = "eastus",
+    [String] $TemplateFile = './scripts/app-service.bicep'
+)
+
+if ($ProductId.Trim() -eq "" ) { 
+    throw "'ProductId' is required, please provide a value for '-ProductId' argument."
+}
+
+if ($SubscriptionId.Trim() -eq "" ) { 
+    throw "'SubscriptionId' is required, please provide a value for '-SubscriptionId' argument."
+}
+
+.\scripts\shared.ps1 -ProductId $ProductId -SubscriptionId $SubscriptionId
+.\scripts\app-service.ps1 -ProductId $ProductId -SubscriptionId $SubscriptionId
+.\scripts\app-service-container.ps1 -ProductId $ProductId -SubscriptionId $SubscriptionId
