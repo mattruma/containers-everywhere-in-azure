be6bb469ae5ae19eaa5dd38ab82055d22b837b21
mattruma <mattruma@gmail.com>
2022-02-03 09:21:10 -0500

    added demo app

---
+++ src/BoardGameNerd.Demo/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BoardGameNerd.Demo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public readonly IConfiguration Configuration;
+
+        public IndexModel(IConfiguration configuration,
+            ILogger<IndexModel> logger)
+{
+            _logger = logger;
+            this.Configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
